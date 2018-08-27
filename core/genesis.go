d10f198da84fd585136123a7c9099e679af4e705
Said Syakin <said.syakin@gmail.com>
2018-08-28 02:10:56 +0400

    Changed ethereum mainnet genesis conf to foodcoin mainnet genesis conf

--- core/genesis.go
+++ core/genesis.go
@@ -298,14 +298,29 @@ func GenesisBlockForTesting(db ethdb.Database, addr common.Address, balance *big
 }
 
 // DefaultGenesisBlock returns the Ethereum main net genesis block.
+//func DefaultGenesisBlock() *Genesis {
+//	return &Genesis{
+//		Config:     params.MainnetChainConfig,
+//		Nonce:      66,
+//		ExtraData:  hexutil.MustDecode("0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"),
+//		GasLimit:   5000,
+//		Difficulty: big.NewInt(17179869184),
+//		Alloc:      decodePrealloc(mainnetAllocData),
+//	}
+//}
+
 func DefaultGenesisBlock() *Genesis {
 	return &Genesis{
 		Config:     params.MainnetChainConfig,
 		Nonce:      66,
-		ExtraData:  hexutil.MustDecode("0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"),
-		GasLimit:   5000,
-		Difficulty: big.NewInt(17179869184),
-		Alloc:      decodePrealloc(mainnetAllocData),
+		ExtraData:  hexutil.MustDecode("0x7575757575757575757575757575757575757575757575757575757575757575"),
+		GasLimit:   16777216,
+		Difficulty: big.NewInt(200000),
+		Alloc: map[common.Address]GenesisAccount{
+			common.HexToAddress("0x9d0153b19d99975f17b5c03ae84c8165e09b7809"): {Balance: new(big.Int).Lsh(big.NewInt(1), 64+20)},
+			common.HexToAddress("0x38482d1dce48e1B4e727ff1a2BE29Ff3b9EF6A53"): {Balance: new(big.Int).Lsh(big.NewInt(1), 64+20)},
+			common.HexToAddress("0x0819da8a19d469CD9593c4449E587d4156cF6065"): {Balance: new(big.Int).Lsh(big.NewInt(1), 64+1)},
+		},
 	}
 }
 
